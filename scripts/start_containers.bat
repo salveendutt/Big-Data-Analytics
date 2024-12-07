a20158574bf0b7bc8f4ff1373dbedac78aa643b0
PrusWielki <prusakpat@gmail.com>
2024-12-07 14:17:11 +0100

    setup batch processing container

--- scripts/start_containers.bat
+++ scripts/start_containers.bat
@@ -1,4 +1,5 @@
 pushd ..
 docker build -t streaming_simulation . -f ./services/streaming_simulation/Dockerfile
 docker build -t streaming_processing . -f ./services/streaming_processing/Dockerfile
+docker build -t batch_processing . -f ./services/batch_processing/Dockerfile
 docker-compose up --remove-orphans
--- services/batch_processing/batch_processing.py
+++ services/batch_processing/batch_processing.py
@@ -1,12 +1,14 @@
 import time
 from cassandra.cluster import Cluster
 from config_batch_processing import cassandra_ip, cassandra_port, cassandra_keyspace
+from cassandra.auth import PlainTextAuthProvider
 
 
 if __name__ == "__main__":
 
     def query_cassandra():
-        cluster = Cluster([cassandra_ip], port=cassandra_port)
+        auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
+        cluster = Cluster([cassandra_ip], port=cassandra_port, auth_provider=auth_provider)
         session = cluster.connect(cassandra_keyspace)
 
         tables = ["dataset1", "dataset2", "dataset3"]
