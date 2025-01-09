ec15a8e5e4671a1a9c7838560534afdae27eba24
Patryk <pruswielki@gmail.com>
2025-01-09 16:59:01 +0100

    cleanup streaming processing

--- services/streaming_processing/config_streaming_processing.py
+++ services/streaming_processing/config_streaming_processing.py
@@ -5,3 +5,6 @@ kafka_connection_attempts_delay = 60
 kafka_topic_processed = "processed_messages"
 spark_master_address = "spark://192.168.1.109:7077"
 spark_jars_packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3,org.apache.kafka:kafka-clients:3.2.3"
+cassandra_username = "cassandra"
+cassandra_password = "cassandra"
+cassandra_keyspace = "fraud_analytics"
