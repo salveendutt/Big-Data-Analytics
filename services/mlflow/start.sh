87401d5633c4d314f9b64afb81a9a67d1ce825ca
Karina Tiurina <rktiurina@gmail.com>
2025-01-20 00:13:25 +0100

    DB setup for mlflow

--- services/mlflow/start.sh
+++ services/mlflow/start.sh
@@ -6,4 +6,5 @@ service cron start
 tail -f /var/log/cron.log &
 
 # Start the MLflow server
-mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root /mlflow/artifacts --host 0.0.0.0
+# mlflow server --backend-store-uri $BACKEND_STORE_URI --default-artifact-root $ARTIFACT_ROOT --host 0.0.0.0
+mlflow server --backend-store-uri sqlite:///mlflow\mlflow.db --default-artifact-root /mlflow/artifacts --host 0.0.0.0
